777c6a91be83789f934edbcad1b7888c37e85f12
Shane Qi <qizengtai@gmail.com>
2019-06-11 23:40:39 -0500

    Initial Commit

---
+++ WithoutAnAccident/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WithoutAnAccident
+//
+//  Created by Shane Qi on 6/11/19.
+//  Copyright © 2019 Shane Qi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
